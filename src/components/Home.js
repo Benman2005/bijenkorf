1db92549b28b579248c7e5fe21ae2e1da6351fe5
Benjamin <benman2005@gMAIL.com>
2018-11-22 00:20:57 +0100

    two different search boxes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,10 @@
 import React, {PureComponent} from 'react'
-import Select from 'react-select'
 
 class Home extends PureComponent{
   state = {
-    query: ' ',
+    query: '',
     select1:null
-
   }
-
   handleInputChange = () => {
     this.setState({
       query: this.search.value,
@@ -16,52 +13,52 @@ class Home extends PureComponent{
      if(this.search.value && this.search.value.length >1){fetch(url, { method: 'GET' })
      .then(res => res.text())
      .then(data => JSON.parse(data))
-     .then(result => result.suggestions.filter(one => one.searchterm.includes(this.state.query)))
+     .then(result => result.suggestions.filter(one => one.searchterm.includes(this.search.value)))
      .then(truien => this.setState({truien: truien}))
-     .then(console.log(this.state.truien))
-     .then(final=> console.log(final))
+     .catch(err=>console.log(err))    
     }
   }
-  // componentDidMount(){
-  //   // const url =`http://localhost:3000/search\?q\=${this.state.query}`
-  //   const url =`https://raw.githubusercontent.com/Benman2005/bijenkorf/noselect/api/_search.get.json`
-  //   // const url =`../../api/_search.get.json` 
-  //   fetch(url, { method: 'GET' })
-  //   .then(res => res.text())
-  //   .then(data => JSON.parse(data))
-  //   .then(result => this.setState({truien: result.suggestions.map(trui=> trui.searchterm)}))
-  // } 
-
+  componentDidMount(){
+    this.search.focus()
+    const url =`https://raw.githubusercontent.com/Benman2005/bijenkorf/noselect/api/_search.get.json`
+    fetch(url, { method: 'GET' })
+    .then(res => res.text())
+    .then(data => JSON.parse(data))
+    .then(result => this.setState({truien2: result.suggestions.map(trui=> trui.searchterm)}))
+    .catch(err=>console.log(err))    
+  } 
   handleChange = select1 => {
     this.setState({select1})
   }
-
   render(){
-    const { select1} = this.state;
     let truien = null
+    let truien2 = null
     if(this.state.truien) truien = this.state.truien.map(trui=>`${trui.searchterm} (${trui.nrResults})`)
+    if(this.state.truien2) truien2 = this.state.truien2.map(trui=>`${trui}`)
     return(
       <div>
       {console.log(truien)}
-      {/* {truien && console.log(truien)} */}
         <h1>Hi, folks</h1>
-        {/* {truien && <Select value={select1} onChange={this.handleChange} options={truien}></Select>} */}
         <form>
           <input
             placeholder="Search for..."
             ref={input => this.search = input}
             onChange={this.handleInputChange}
-            />
-            <table>
-            {truien && truien.map(trui=>{return <tr><td>{trui}</td></tr>})}
-            </table>
-            {/* <ul>{truien && truien.map(trui=>{return <li>{trui}</li>})}</ul> */}
+          />
+          <table>
+            <tbody>
+              {truien && truien.map(trui=>{return <tr key={trui}><td>{trui}</td></tr>})}
+            </tbody>
+          </table>
           {/* <p>{this.state.query}</p> */}
         </form>
+        <input list="truien" />
+        <datalist id="truien">
+          {truien2 && truien2.map(trui=>{return <option key={trui} value={trui}></option>})}
+        </datalist>
       </div>
     )
   }
-
 }
 
 export default Home
