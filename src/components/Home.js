053144e04d656c29cd277dd2fddf946813ee3723
Benjamin <benman2005@gMAIL.com>
2018-11-21 17:13:29 +0100

    WITH REACT-SELECT

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,58 +2,31 @@ import React, {PureComponent} from 'react'
 import Select from 'react-select'
 
 class Home extends PureComponent{
-  state = {
-    query: '',
-    select1:null
-
-  }
-
-  handleInputChange = () => {
-    this.setState({
-      query: this.search.value,
-    })
-     const url =`https://raw.githubusercontent.com/Benman2005/frontend-dev-assignment/master/api/_search.get.json`
-     fetch(url, { method: 'GET' })
-     .then(res => res.text())
-     .then(data => JSON.parse(data))
-     .then(result => result.suggestions.filter(one => one.searchterm.includes(this.state.query)))
-     .then(truien => this.setState({truien: truien}))
-     .then(console.log(this.state.truien))
-     .then(final=> console.log(final))
-  }
   componentDidMount(){
-    // const url =`http://localhost:3000/search\?q\=${this.state.query}`
     const url =`https://raw.githubusercontent.com/Benman2005/frontend-dev-assignment/master/api/_search.get.json`
-    // const url =`../../api/_search.get.json` 
     fetch(url, { method: 'GET' })
     .then(res => res.text())
     .then(data => JSON.parse(data))
-    .then(result => this.setState({truien: result.suggestions.map(trui=> trui.searchterm)}))
+    .then(result => this.setState({truien: result.suggestions.map(trui=> `${trui.searchterm} ( ${trui.nrResults} )`)}))
   } 
-
   handleChange = select1 => {
     this.setState({select1})
   }
-
   render(){
     const { select1} = this.state;
     let truien = null
     if(this.state.truien) truien = this.state.truien.map(trui=>{return {value:trui, label: trui}})
+    const customStyles = {
+      control: () => ({
+        width: "80vmin",
+        border: '2px solid black'
+      })
+    }
     return(
       <div>
-      {console.log(this.state.truien)}
-      {/* {truien && console.log(truien)} */}
+      {/* {console.log(this.state.truien)} */}
         <h1>Hi, folks</h1>
-        {truien && <Select value={select1} onChange={this.handleChange} options={truien}></Select>}
-
-        <form>
-          <input
-            placeholder="Search for..."
-            ref={input => this.search = input}
-            onChange={this.handleInputChange}
-          />
-          <p>{this.state.query}</p>
-        </form>
+        {truien && <Select styles={customStyles} value={select1} onChange={this.handleChange} options={truien}></Select>}
       </div>
     )
   }
